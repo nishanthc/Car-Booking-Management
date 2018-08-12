f77a5cda190b36b2b77cd973647f7333f2eae56a
Nish chandradas <nishanthchandradas@gmail.com>
2018-08-12 19:21:52 +0100

    Added Core Unit Test

--- init.py
+++ init.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
-
+import os
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -12,10 +12,11 @@ from users import users
 app.register_blueprint(core)
 app.register_blueprint(users)
 csrf = CSRFProtect(app)
-
+db_file_name = "test.db"
 app.config['SECRET_KEY'] = 'fsdfsdfdsf'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/Nish/Development/Tutor_Booking_Management/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                                                os.path.join(app.root_path, db_file_name)
 
 if __name__ == '__main__':
     app.run()
---
+++ tests/test_core.py
@@ -0,0 +1,41 @@
+import os
+import unittest
+
+
+from init import app
+from models import db
+
+TEST_DB = 'test2.db'
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                                                os.path.join(app.root_path, TEST_DB)
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
