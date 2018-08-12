a1da21eb2b0cdc5b2efe9b8e5d05877c02546a15
Nish chandradas <nishanthchandradas@gmail.com>
2018-08-12 18:39:14 +0100

    Refactored

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask,render_template,redirect,flash,url_for,request
-from models import db,User
-from init import app
-from forms import RegistrationForm, LoginForm
+from flask import Flask, render_template, redirect, flash, url_for, request
 from flask_login import current_user, login_user, logout_user
-from passlib.hash import pbkdf2_sha256
+from forms import RegistrationForm, LoginForm
+from init import app
+from models import db, User
+
+
 @app.route('/')
 def home():
+    return render_template('home.html', title='Hello!')
 
-    return render_template('home.html',title = 'Hello!')
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
@@ -15,16 +16,17 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username = form.username.data,
-                    email = form.email.data,
-                    mobile = form.mobile.data,
-                    password = form.password.data)
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    mobile=form.mobile.data,
+                    password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Thanks for registering')
         return redirect(url_for('login'))
 
-    return render_template('register.html',title = 'Register',form=form)
+    return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
@@ -32,8 +34,8 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        login_data = User(username = form.username.data,
-                    password = form.password.data)
+        login_data = User(username=form.username.data,
+                          password=form.password.data)
         user = User.query.filter_by(username=login_data.username.lower()).first()
         if user:
             if user.password == login_data.password.secret:
@@ -43,15 +45,14 @@ def login():
                 flash('Invalid login')
         else:
             flash('Invalid login')
+    return render_template('login.html', title='Login', form=form)
 
-
-
-    return render_template('login.html',title = 'Login',form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run()
--- forms.py
+++ forms.py
@@ -1,19 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField ,validators
-from wtforms.validators import DataRequired
-from wtforms_alchemy import Unique
-
-from wtforms_alchemy import ModelForm
 from models import User
+from wtforms import StringField, PasswordField, IntegerField, validators
+from wtforms.validators import DataRequired
+from wtforms_alchemy import Unique, ModelForm
+
+
 class RegistrationForm(ModelForm, FlaskForm):
-    username = StringField('Username', validators=[DataRequired(),Unique(User.username,
-                        message="User with username exists")])
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Unique(User.username,
+                                                          message="User with username exists")])
+
     email = StringField('Email', validators=[DataRequired(),
-            validators.Email(message="Must be a valid email address"),
-            Unique(User.email,message="User with email exists")])
+                                             validators.Email(message="Must be a valid email address"),
+                                             Unique(User.email, message="User with email exists")])
+
     mobile = IntegerField('Mobile', validators=[DataRequired()])
+
     password = PasswordField('Password', validators=[DataRequired(),
-        validators.EqualTo('confirm_password', message='Passwords must match')])
+                                                     validators.EqualTo('confirm_password',
+                                                                        message='Passwords must match')])
     confirm_password = PasswordField('Repeat Password')
 
 
