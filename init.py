5f39aa07deeacbcaeb15de343d43d7cc4f5d9683
Nishanth Chandradas <nishanth.chandradas@ts.catapult.org.uk>
2018-08-14 14:01:29 +0100

    Fixed DB config issues

--- init.py
+++ init.py
@@ -3,19 +3,22 @@ from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
 import os
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+
 db_file_name = "tbs.db"
 test_db_file_name = "test_database.db"
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
                                                 os.path.join(app.root_path, db_file_name)
 app.config['TEST_SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
                                                 os.path.join(app.root_path, test_db_file_name)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'fsdfsdfdsf'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
 db.create_all()
 from core import core
 from admin import admin
